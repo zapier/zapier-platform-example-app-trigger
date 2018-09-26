0c751c809778e4d9748fdf6cc3c187f86a6ec9cf
stevemolitor <stevemolitor@gmail.com>
2018-09-26 16:19:38 -0500

    add link to dynamic fields example

--- triggers/recipe.js
+++ triggers/recipe.js
@@ -60,6 +60,8 @@ module.exports = {
     //   outputFields: [
     //    () => { return []; }
     //   ]
+    // For a more complete example of using dynamic fields see
+    // https://github.com/zapier/zapier-platform-cli#customdynamic-fields.
     // Alternatively, a static field definition should be provided, to specify labels for the fields
     outputFields: [
       {key: 'id', label: 'ID'},
